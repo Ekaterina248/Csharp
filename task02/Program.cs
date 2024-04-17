cab1a82083aebee7343ea4a74193fdff447992f8
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-17 22:24:41 +0700

    решила задачу task02

---
+++ task02/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая на входе принимает 2 числа и выдет какое число больше,
+ какое меньше
+ */
+Console.Write("ВВедите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("ВВедите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+{
+    Console.Write("Первое число больше");
+}
+else
+{
+    Console.Write("Второе число больше");
+}
