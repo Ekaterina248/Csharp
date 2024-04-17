8402fe2189b3b6dfe6f950ba3f019d2781336c2c
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-17 23:26:16 +0700

    решила task08

---
+++ task08/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая на входе принимает число N,
+на выходе показывает все четные числа от 1 до N
+ */
+Console.Write("ВВедите положительное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=number; i=i+1)
+{
+    Console.Write(i+" ");
+}
