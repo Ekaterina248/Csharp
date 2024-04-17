fa54508439c208049de612da20b56e32b244cdc0
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-17 21:58:24 +0700

    решила задачу task00

---
+++ task00/Program.cs
@@ -0,0 +1,4 @@
+﻿// Прокрамма, которая на вход принимает числа, на выход выдает квадрат
+Console.WriteLine("ВВедие число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number*number);
