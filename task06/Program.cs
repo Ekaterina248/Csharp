742e7e1a2258a9c8a833aeed32ae26d65631fd07
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-17 23:13:53 +0700

    решила task06

---
+++ task06/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на входе принимает число,
+ и выдает является ли число четным
+ */
+Console.Write("ВВедите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2==1)
+{
+    Console.Write("Число " +number+" нечетное");
+}
+else
+{
+    Console.Write("Число " +number+" четное");
+}
