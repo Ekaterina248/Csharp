890d216d5274adfc9ae9e47b7efdbab6d94a13c0
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-18 23:12:37 +0700

    решили задачу task12

---
+++ task12/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+напишие программу которая будет принимать на вход два числа и проверять является одно число кратно другому
+Если второе число не кратно первому, программа выводит остаток от делнеия
+
+*/
+
+Console.WriteLine("Введите 2 числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("первое число: " + number1+"; второе число: " + number2);
+if (number1%number2==0)
+{
+    Console.Write("Второе число кратно первому ");
+}
+else
+{
+    Console.Write("остаток от деления первого числа на второе равен " + number1%number2);
+}
