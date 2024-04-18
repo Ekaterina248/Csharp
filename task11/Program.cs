455458fd2e56013039ef049e30dc5b8a636f166b
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-18 22:52:19 +0700

    решили задачу11

---
+++ task11/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+напишие программу которая выводит трехзначное число и удаляет вторую цифру этого числа
+456->46
+782->72
+
+*/
+
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+int first=number/100;
+int second=number%10;
+Console.WriteLine(first*10+second);
