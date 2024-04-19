aacd99d4cd4dd69e0227ae2f22560e8393c59deb
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-19 21:54:13 +0700

    решила task17

---
+++ task17/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает натуральное число N,
+ а на выходе показывает его цифры через запятую.*/
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int celoe=number;
+while (celoe>10)
+{
+    Console.Write(celoe%10+", ");
+    celoe=celoe/10;
+
+}
+Console.Write(celoe+".");
