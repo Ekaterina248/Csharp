bb251e219717d81f8816233ca4e2baab53576e76
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-21 23:54:08 +0700

    добавила комментарии в  task42

---
+++ task42/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Лекция5. Рекурсии
+Задача на нахождение факториала*/
+
+int Fact(int n)
+{
+    if (n == 1 || n==0)
+    {
+        return 1;
+    }
+
+    return n * Fact(n - 1);
+}
+
+System.Console.WriteLine(Fact(4));
