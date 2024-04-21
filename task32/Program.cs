9b61398960017939a9170dca5d290ad63dda095d
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-21 16:49:15 +0700

    изменила  task32 пример из лекции на break

--- task32/Program.cs
+++ task32/Program.cs
@@ -2,7 +2,7 @@
 в случае наличия в массиве нечетного числа*/
 
 Console.WriteLine("START");
-int[] numbers = {2,4,6,4,8,10};
+int[] numbers = {2,4,6,7,8,10};
 foreach (int n in numbers)
 {
     if (n%2==1)
@@ -10,5 +10,9 @@ foreach (int n in numbers)
         Console.WriteLine("нечетное число найдено");
         break;
     }
+    else
+     {
+        Console.WriteLine($"элемент массива {n} - четное число");
+     }
 }
 Console.WriteLine("STOP");
