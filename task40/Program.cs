20e2b3e4197472ef50a0a362db53c1a4e7a757e6
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-21 22:54:43 +0700

    решила  task40

---
+++ task40/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Лекция5. Рекурсия*/
+
+void OpenMatreshka(int size)
+{
+    System.Console.WriteLine($"Открой матрешку размером {size} ...");
+    if (size == 1)
+    {
+        System.Console.WriteLine("открыта самая маленькая матрешка");
+        return;
+    }
+
+    OpenMatreshka(size - 1);
+}
+
+OpenMatreshka(5);
