bb9f5deb5420b377f200f03d836054c498834e63
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-21 23:26:51 +0700

    добавила комментарии в  task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -2,14 +2,15 @@
 
 void OpenMatreshka(int size)
 {
-   if (size == 1)
+   if (size == 1) //базовый случай, когда нужно выйти из функции
     {
      System.Console.WriteLine("открыта самая маленькая матрешка");
      return;
     }
    
+   System.Console.WriteLine($"Открыта матрешка размером {size} ..."); // напечатает от большего к меньшему, т.к. до вызова рекурсии
    OpenMatreshka(size - 1);
-   System.Console.WriteLine($"Открыта матрешка размером {size} ...");
+   //System.Console.WriteLine($"Открыта матрешка размером {size} ...");//напечатает от меньшего к большему, т.к. после вызова рекурсии
   
 }
 
