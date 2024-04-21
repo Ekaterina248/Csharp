462d226367e982301552c483d42aef9ad86117b2
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-21 16:43:53 +0700

    решила  task33 пример из лекции на continue

---
+++ task33/Program.cs
@@ -0,0 +1,12 @@
+﻿/* пример из лекции по continue*/
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Итерация {i}:");
+    if (i==3)
+    {
+        Console.WriteLine("3");
+        continue;
+    }
+    Console.WriteLine("До этого момента не дошел цикл for при i=3, но продолжил свою работу");
+}
