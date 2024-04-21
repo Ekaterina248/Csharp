243b5a62cf3cd5f4a68b029851aca9677e370de1
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-21 20:15:08 +0700

    решила  task35

---
+++ task35/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задайте двумерный массив. Найдите сумму элементов,
+находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+Пример
+2 3 4 3
+4 3 4 1 => 2 + 3 + 5 = 10
+2 9 5 4
+
+*/
+int[,] Fill2Array(int row, int colum) // рендом 2-массива
+{
+    int[,] arr = new int[row, colum];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+
+        }
+    }
+    return arr;
+}
+
+void Print2Array(int[,] arr)// печать 2-массива
+{ for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j]+"\t");
+
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+int SumDiagonaly(int[,] arr)
+{
+    int sum=0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i==j)
+            {sum=sum+arr[i,j];}
+        }
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Введите кол-во строк и столбцов в 2-м массиве:");   
+int r = Convert.ToInt32(Console.ReadLine()); 
+int c = Convert.ToInt32(Console.ReadLine()); 
+int[,] matrix=Fill2Array(r,c);
+Print2Array(matrix);
+System.Console.Write("сумма элементов диагонали массива= ");
+int sumd=SumDiagonaly(matrix);
+Console.Write(sumd);
