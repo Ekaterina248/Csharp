bba50076767c7acbce2dd19df6afc15884e3d83a
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-21 16:33:35 +0700

    решила  task32

---
+++ task32/Program.cs
@@ -0,0 +1,14 @@
+﻿/*выдать на экран сообщение 
+в случае наличия в массиве нечетного числа*/
+
+Console.WriteLine("START");
+int[] numbers = {2,4,6,4,8,10};
+foreach (int n in numbers)
+{
+    if (n%2==1)
+    {
+        Console.WriteLine("нечетное число найдено");
+        break;
+    }
+}
+Console.WriteLine("STOP");
