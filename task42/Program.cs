678479f99143079cac423b8795e923bdbf468504
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-22 00:08:55 +0700

    изменила task42

--- task42/Program.cs
+++ task42/Program.cs
@@ -5,10 +5,12 @@ int Fact(int n)
 {
     if (n == 1 || n==0)
     {
+        Console.WriteLine($"Stop: {n}");
         return 1;
     }
-
+    System.Console.WriteLine(n);
     return n * Fact(n - 1);
+    //Console.WriteLine(n); // не работает раскручивание!!!!???
 }
 
-System.Console.WriteLine(Fact(4));
+System.Console.WriteLine(Fact(5));
