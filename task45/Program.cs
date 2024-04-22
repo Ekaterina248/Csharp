8d4262b03d93f4760be5a86ebe81579a0b3b6c2e
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-22 23:13:04 +0700

    решила семинар_6.зад2._task45

---
+++ task45/Program.cs
@@ -0,0 +1,11 @@
+﻿/*task 44. Задача2. Семинар 6
+На основе символов строки (тип string) сформировать массив
+символов (тип char[]). Вывести массив на экран.*/
+
+string st="Hello!";
+char[] charArr=new char [st.Length];
+for (int i = 0; i < st.Length; i++)
+{
+    charArr[i]=st[i];
+    System.Console.Write($"'{charArr[i]}', "); 
+}
