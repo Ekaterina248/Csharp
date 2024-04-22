6d1a2a8fbbad02f8b1bc6ba1fb82586a7791265c
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-23 00:08:56 +0700

    не решила семинар_6.зад3._task46

---
+++ task46/Program.cs
@@ -0,0 +1,34 @@
+﻿/*task 46. Задача3. Семинар 6
+Считать строку с консоли, состоящую из латинских
+букв в нижнем регистре. Выяснить, сколько среди
+введённых букв гласных.
+
+*/
+Console.WriteLine("введите строку состоящую из латинских букв в нижнем регистре: ");
+//string st = (Console.ReadLine());
+string st="hellow";
+
+char[] charArr=new char [st.Length];
+int sumglas=0;
+for (int i = 0; i < st.Length; i++)
+{
+    charArr[i]=st[i];
+    /* if (charArr[i]==e);//как задать условие???
+    {
+        sumglas=sumglas+1;
+    } */
+    System.Console.Write($"'{charArr[i]}', "); 
+}
+System.Console.WriteLine();
+System.Console.WriteLine(charArr[1].CompareTo('t'));
+System.Console.WriteLine(charArr[1].CompareTo('h'));
+System.Console.WriteLine(charArr[1].CompareTo('l'));
+System.Console.WriteLine(charArr[1].CompareTo('r'));
+System.Console.WriteLine();
+System.Console.WriteLine($"сумма гласных = {sumglas}"); 
+/*for (int i = 0; i < st.Length; i++)
+{
+    if (st.Contains('e');
+}
+
+System.Console.WriteLine(st.Contains('e'));*/
