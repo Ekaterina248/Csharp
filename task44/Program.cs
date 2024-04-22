4085b9e298aab7edb8759aff165f0b50b25b0fd7
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-22 23:01:56 +0700

    решила семинар_6.зад1._task44

---
+++ task44/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+task 44. Задача1. Семинар 6. 
+Задайте массив символов (тип char []). Создайте строку из
+символов этого массива. 
+
+*/
+
+char[] charArr = {'a','b','c','d'};
+//string str=new string (charArr);
+string str="";
+for (int i = 0; i <charArr.Length; i++)
+{
+    str=str+charArr[i];
+}
+
+
+System.Console.WriteLine(str);
