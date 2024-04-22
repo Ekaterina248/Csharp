dc6536af65dd9fbf2c6ab509bd2c6a9b942dd345
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-23 01:40:35 +0700

    решила  ДЗ2 по строкам task48

---
+++ task48/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Урок 6. Массивы и строки.ДЗ_2
+Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, 
+в которой все заглавные буквы заменены на строчные
+
+“aBcD1ef!-” => “abcd1ef!-” */
+
+/* char ch='D';
+System.Console.WriteLine(Char.IsLower(ch));//проверяет строчную 
+System.Console.WriteLine(Char.IsUpper(ch));//проверяет заглавную */
+
+string st="aBcD1ef!-";
+string st2="";
+char[] charArr=new char [st.Length];
+for (int i = 0; i < st.Length; i++)
+{
+    charArr[i]=st[i];
+    if (Char.IsUpper(charArr[i]))
+    {
+       charArr[i]=Char.ToLower(charArr[i]);
+    }
+    st2=st2+charArr[i];
+    //System.Console.Write($"'{charArr[i]}', "); //печать массива
+}
+System.Console.Write(st2);
