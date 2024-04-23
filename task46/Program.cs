e8ab1044eb43fd3ba811f9f7748cedcef86ce51b
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-23 13:22:07 +0700

    семинар6. откорректировала задачу task46

--- task46/Program.cs
+++ task46/Program.cs
@@ -7,28 +7,14 @@
 Console.WriteLine("введите строку состоящую из латинских букв в нижнем регистре: ");
 //string st = (Console.ReadLine());
 string st="hellow";
-
-char[] charArr=new char [st.Length];
+string glasny="eyuioa";
 int sumglas=0;
+
 for (int i = 0; i < st.Length; i++)
 {
-    charArr[i]=st[i];
-    /* if (charArr[i]==e);//как задать условие???
-    {
-        sumglas=sumglas+1;
-    } */
-    System.Console.Write($"'{charArr[i]}', "); 
+ for (int j=0; j<glasny.Length; j++)
+ {
+   if (st[i]==glasny[j]) ;
+   strnum=strnum+1;
+ }
 }
-System.Console.WriteLine();
-System.Console.WriteLine(charArr[1].CompareTo('t'));
-System.Console.WriteLine(charArr[1].CompareTo('h'));
-System.Console.WriteLine(charArr[1].CompareTo('l'));
-System.Console.WriteLine(charArr[1].CompareTo('r'));
-System.Console.WriteLine();
-System.Console.WriteLine($"сумма гласных = {sumglas}"); 
-/*for (int i = 0; i < st.Length; i++)
-{
-    if (st.Contains('e');
-}
-
-System.Console.WriteLine(st.Contains('e'));*/
