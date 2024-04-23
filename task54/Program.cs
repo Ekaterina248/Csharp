f02443d697097856af2a4a1a556cfb2d7f531640
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-23 22:32:31 +0700

    семинар7.рекурсия.задача на возведениее в степень task54

---
+++ task54/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишие программу, которая на вход принимает два числа А и В,
+и возводит число A в целую степень В спомощью рекурсии
+
+*/
+
+int PowUser (int a, int b)
+{
+    if (b==1)
+    {
+        return a;
+    }
+    else
+    {
+        return a* PowUser(a,b-1);
+    }
+    
+
+}
+
+System.Console.WriteLine(PowUser(4,2));
+
