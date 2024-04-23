3bc74475a2e978808aaf846137622fde680f4cf6
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-24 00:29:23 +0700

    Промежуточная аттестация.Задача2. _task57

--- task57/Program.cs
+++ task57/Program.cs
@@ -7,18 +7,20 @@ n = 2, m = 3 -> A(n,m) = 29
 
 int Akkerman(int n, int m)
 {
-    if (n==0)
+    if (n == 0)
     {
-      return m+1;
+        return m + 1;
     }
-    else if (m==0)
+    else if (m == 0)
     {
-        return Akkerman(n-1,1);
+        return Akkerman(n - 1, 1);
     }
-    else 
+    else
     {
-        return Akkerman(n-1, Akkerman(n,m-1));
+        return Akkerman(n - 1, Akkerman(n, m - 1));
     }
 }
- 
- System.Console.WriteLine(Akkerman(3,2));
+int akN =2 ;
+int akM = 5;
+
+System.Console.WriteLine($"Функция Аккермана  A({akN},{akM})= {Akkerman(akN,akM)}");
