53cf643d9e73a54eff1fc383d94b65d6db9b54f2
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-23 13:43:29 +0700

    семинар6. решила  задачу3- task46

--- task46/Program.cs
+++ task46/Program.cs
@@ -4,17 +4,22 @@
 введённых букв гласных.
 
 */
-Console.WriteLine("введите строку состоящую из латинских букв в нижнем регистре: ");
+//Console.WriteLine("введите строку состоящую из латинских букв в нижнем регистре: ");
 //string st = (Console.ReadLine());
-string st="hellow";
-string glasny="eyuioa";
-int sumglas=0;
+    string st="аоия777ккeky";
+    string glasny="eyuioaуеыаоэяию";
+    int sumglas=0;
 
-for (int i = 0; i < st.Length; i++)
-{
- for (int j=0; j<glasny.Length; j++)
- {
-   if (st[i]==glasny[j]) ;
-   strnum=strnum+1;
- }
-}
+    for (int i = 0; i < st.Length; i++)
+    {
+    for (int j=0; j<glasny.Length; j++)
+    {
+      if (st[i]==glasny[j])
+      {
+        sumglas=sumglas+1;
+      }
+    }
+    }
+    
+    Console.WriteLine($"кол-во гласных букв в строке'{st}' равно {sumglas}");
+    
