daaa02e65a4bd50d5d2ac65747de6ebb0abf184a
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-23 14:02:15 +0700

    семинар7.рекурсия.задача1_ task51

---
+++ task51/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Семинар7. Рекурсии
+Задача на нахождение факториала*/
+
+/* int Fact(int n)
+{
+    if (n == 1 || n==0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    System.Console.WriteLine(n);
+    return n * Fact(n - 1);
+    //Console.WriteLine(n); // не работает раскручивание!!!!???
+}
+
+System.Console.WriteLine(Fact(5)); */
+//_____________________________________________
+
+/*Семинар7. Рекурсии
+Напишите программу, которая будет принимать на вход число и
+возвращать сумму его цифр.
+Пример
+123 => 6
+63 => 9
+*/
+int SumNumber(int num)
+{
+  if (num==0)
+  {
+    return 0;
+   }
+   else 
+   {
+    return num%10+SumNumber(num/10);
+   }
+}
+
+int number=991133;
+System.Console.WriteLine(SumNumber(number));
+
+
