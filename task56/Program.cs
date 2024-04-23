53f1e15b38185e7e34a6d831433b1a82d25dcbb1
Ekaterina Shipunova <odegda@rtr22.ru>
2024-04-24 00:31:18 +0700

    Промежуточная аттестация.Задача1. _task56_(отформатировала)

--- task56/Program.cs
+++ task56/Program.cs
@@ -7,17 +7,17 @@ M = 1; N = 5 -> "1, 2, 3, 4, 5"
 M = 4; N = 8 -> "4, 5, 6, 7, 8"
 */
 System.Console.WriteLine("задайте границы отрезка: ");
-int n1=Convert.ToInt32(Console.ReadLine());
-int n2=Convert.ToInt32(Console.ReadLine());
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
 
-void IntegerOtrezok (int m, int n)
+void IntegerOtrezok(int m, int n)
 {
-   if (m==n+1)
-   {
-    return;
-   }
-   System.Console.Write($"{m} ");
-   IntegerOtrezok(m+1,n);
+    if (m == n + 1)
+    {
+        return;
+    }
+    System.Console.Write($"{m} ");
+    IntegerOtrezok(m + 1, n);
 }
 System.Console.Write("Числа в отрезке: ");
-IntegerOtrezok(n1,n2);
+IntegerOtrezok(n1, n2);
